7f966bf324df4db76ee8e92a7ea066c5d1c109d8
Hoa Vy <vycoi1511@gmail.com>
2024-06-01 20:45:02 +0700

    update api url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://sea-lion-app-5yl6a.ondigitalocean.app';
+export const API_URL = window._env_.VITE_API_URL || 'https://sea-lion-app-5yl6a.ondigitalocean.app';
 export const NOTIFICATIONS_HUB = 'hubs/notifications';
 export const ADDRESS_API_URL = 'https://city-and-state-search-api.p.rapidapi.com';
 // export const API_URL = 'https://hci-be-new.azurewebsites.net';
